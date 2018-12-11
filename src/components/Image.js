29517764791e6e3abcea6e9f15290530c9e90594
jakaardiola@gmail.com <jakaardiola@gmail.com>
2018-12-11 22:11:48 +0700

    props pada class component

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class Image extends Component {
     render() {
         return (
-            <img src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/food1.jpg" width="500px" alt="gambar ae"></img>
+            <img src={this.props.linkgambar} width={this.props.lebar} alt="gambar ae"></img>
         );
     }
 }
