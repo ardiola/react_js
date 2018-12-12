7e10181267d018de0b2407b784135719fb18233f
jakaardiola@gmail.com <jakaardiola@gmail.com>
2018-12-12 21:49:11 +0700

    Props pada Class Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    constructor(){
+        super();
+        this.state = {
+            daftar: "daftar makanan nusantara"
+        }
+    }
     render() {
         return(
             <div>
                 <h2>tes render elements</h2>
+                <p>{this.state.daftar}</p>
             </div>
         );
     }
