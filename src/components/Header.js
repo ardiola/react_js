21b45549cf5b2b5a613ce2b8457ffd47862b5d93
jakaardiola@gmail.com <jakaardiola@gmail.com>
2018-12-18 23:09:41 +0700

    Handling Events dan Parameters

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
     constructor(props){
         super(props);
         this.state = {
             daftar: "daftar makanan nusantara",
             daftarList: this.props.list
         }
+        this.handlePesan = this.handlePesan.bind(this); //untuk memanggil state apabila pake ini
     }
+
+    handlePesan(value, e) {
+        e.preventDefault();
+        alert(this.state.daftar);
+        alert(value);
+    }
+
     render() {
         return(
             <div>
                 <h2>tes render elements</h2>
                 <p>{this.state.daftar}</p>
                 <p>{this.state.daftarList}</p>
+                <a href="/" onClick={(e) => this.handlePesan("Pesan Dari Header", e)}>Halaman Header</a> 
             </div>
         );
     }
 }
 
 export default Header;
+
+// () => untuk memanggil function
--- src/components/Top.js
+++ src/components/Top.js
@@ -3,14 +3,19 @@ import React from "react";
 
 const Top = () => {
 
-    function handlePesan() {
+    const handlePesan = (value, e) => {
+        e.preventDefault();
         alert("ini halaman top!!");
+        alert(value)
     }
 
     return (
-        <a href="/" onClick={handlePesan} >Halaman Top</a>
+        <a href="/" onClick={(e) =>handlePesan("Pesan Dari Top", e)} >Halaman Top</a>
     );
 }
 
 
 export default Top;
+
+//const handlePesan = () => penulisan fucntion
+// bisa juga function
